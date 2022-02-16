bed6d6c335d5f0277b8142819ce980f2e1a3b1e0
Rahmeh AlOmary <Rahmeh.AlOmary@jo.parasolutions.com>
2022-02-16 14:25:40 +0200

    larg screen

---
+++ js/main.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+
+    $('ul.tabs li').click(function () {
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+        $('.current').css('animation', 'animation 1s ease-in-out');
+
+
+    })
+
+})
+
+
+$('.toggle_points li').click(function (e) {
+    $('img.glasses.active').fadeOut().removeClass('active');
+    $( '#' + $(this).data('toggleTarget') ).fadeIn().toggleClass('active');
+});
+
+
+
+$(window).resize(function () {
+
+    if ($(window).width() <= 768) {
+
+        $('.tab-content').removeClass('current');
+   
+
+    }
+
+});
+$('.back-btn').click(function (e) {
+    $('.tab-content').removeClass('current');
+});
